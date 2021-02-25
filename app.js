267d89d7bf25ba338e35967ad5fa1be5155f5b9d
Rafael Guglielmi <rafaelcg@gmail.com>
2021-02-25 00:30:20 +0000

    updated port to be dynamic

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 routes(app);
 
-var server = app.listen(3000, function(){
+const port = process.env.port || 3000
+
+var server = app.listen(port, function(){
     console.log("app running on port.", server.address().port);
 });
